d1358f895bcbe08b9b18910a45d615b389ce6ed3
rupert <conveyr@qq.com>
2013-09-17 17:22:00 +0800

    zh languge

---
+++ js/const-zh.js
@@ -0,0 +1,39 @@
+var CONST = {};
+CONST.level1Hint = "你好！火车游戏带你畅游世界了！";
+CONST.level4Hint = "你已经进入等级4！非常了不起！不过游戏的难度才刚刚开始！";
+CONST.level7Hint = "你已经进入等级7！最终的胜利就在眼前！";
+CONST.successText = "恭喜！你已获得最后的胜利！";
+CONST.language = {btn_back:"返回",control_audio:"游戏声音",control_music:"音乐",credits_btn:"credits",level_text:"Level",begin_text:"开始",btn_next:"进入"};
+CONST.words = [
+    "ant",
+    "an",
+    "as",
+    "banana",
+    "boy",
+    "botton",
+    "cat",
+    "cake",
+    "dog",
+    "hat",
+    "is",
+    "king",
+    "keep",
+    "kindergarden",
+    "lion",
+    "milk",
+    "many",
+    "quick",
+    "queen",
+    "red",
+    "radio",
+    "run",
+    "random",
+    "rude",
+    "ruby",
+    "small",
+    "tiger",
+    "win",
+    "yellow",
+    "zebra",
+    "zoo"
+];
